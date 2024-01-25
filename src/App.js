655a5d54d7361d5295a720ae29b9c38547fa669f
20031019saikiranbugade <saikiranbugade2002@gmail.com>
2024-01-25 21:10:15 +0530

    Changes saved

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
+// import React from 'react'
+
+// function App() {
+//   const success = (position) => {
+//     console.log(position)
+//     const { latitude, longitude } = position.coords;
+//     console.log("Latitude:" + latitude + "\n" + "Longitude:" + longitude);
+//     console.log(position.timestamp)
+//   }
+//   const error = (error) => {
+//     console.log("There is an error " + error)
+//   }
+//   function getLocation(e) {
+//     const result = navigator.geolocation.getCurrentPosition(success, error);
+//       }
+//   return (
+//     <>
+//       <div style={{ margin: 20 }}>
+//         <button onClick={getLocation}>Get Current Location</button>
+//       </div>
+//     </>
+//   )
+// }
+
+// export default App
+
+
 import React from 'react'
 
 function App() {
-  const success = (position) => {
-    console.log(position)
-    const { latitude, longitude } = position.coords;
-    console.log("Latitude:" + latitude + "\n" + "Longitude:" + longitude);
-    console.log(position.timestamp)
-  }
-  const error = (error) => {
-    console.log("There is an error " + error)
-  }
-  function getLocation(e) {
-    const result = navigator.geolocation.getCurrentPosition(success, error);
-      }
   return (
     <>
-      <div style={{ margin: 20 }}>
-        <button onClick={getLocation}>Get Current Location</button>
-      </div>
+    
     </>
   )
 }
