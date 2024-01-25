3cf6736d6aad9a438a2f67314ca1005b898b7a02
20031019saikiranbugade <saikiranbugade2002@gmail.com>
2024-01-25 20:43:37 +0530

    first commited by me

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
+  const success = (position) => {
+    console.log(position)
+    const { latitude, longitude } = position.coords;
+    console.log("Latitude:" + latitude + "\n" + "Longitude:" + longitude);
+    console.log(position.timestamp)
+  }
+  const error = (error) => {
+    console.log("There is an error " + error)
+  }
+  function getLocation(e) {
+    const result = navigator.geolocation.getCurrentPosition(success, error);
+      }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div style={{ margin: 20 }}>
+        <button onClick={getLocation}>Get Current Location</button>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
