bf9be431e64688a2f87d60150512a6605a3ff374
20031019saikiranbugade <saikiranbugade2002@gmail.com>
2024-01-27 20:28:18 +0530

    Changes

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+    <h3>Hello Components</h3>
+    </>
+  )
+}
+
+export default Home
---
+++ src/container/HomeComponents.js
@@ -0,0 +1,3 @@
+import Home from "../components/Home";
+import {connect} from 'react-redux'
+export default Home;
